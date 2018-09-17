4b6a16f3cf5b8b30213e0adc402593f615b55a95
D7705 <D7705@DESKTOP-T1C7S27>
2018-09-17 11:59:49 +0900

    주차별 과제를 추가하였음.

--- exBean/src/exBean/studentDAO.java
+++ exBean/src/exBean/studentDAO.java
@@ -1,5 +1,6 @@
 package exBean;
 import java.sql.*;
+import java.util.*;
 public class studentDAO {
  public void insert(studentVO item) throws Exception {
   String no = item.getNo();
@@ -13,4 +14,19 @@ public class studentDAO {
   sql.setString(2,name);
   sql.executeUpdate();
  }
+ public List<studentVO> select() throws Exception {
+	  List<studentVO> items = new ArrayList<studentVO>();
+	  Class.forName("oracle.jdbc.driver.OracleDriver");
+	  Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201800001","201800001");
+	  PreparedStatement sql = conn.prepareStatement("select * from student");
+	  ResultSet rs = sql.executeQuery();
+	  while(rs.next()) {
+	   studentVO item = new studentVO();
+	   item.setNo(rs.getString("no"));
+	   item.setName(rs.getString("name"));
+	   items.add(item);
+	   //System.out.print(items);
+	  } 
+	  return items;
+	 }
 }
